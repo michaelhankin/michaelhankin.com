27238ba3ef8e6026b2987b66e7e1a80cbf88cee9
Michael Hankin <mjhankin1@gmail.com>
2020-01-20 17:12:57 -0600

    add icon and og image

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,19 @@ module.exports = {
     description: `Software Engineer`,
     author: `@michaelhankin`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Michael Hankin`,
+        icon: `static/icon.png`,
+        start_url: `/`,
+        background_color: `#f0f0f0`,
+        theme_color: `#3273dc`,
+        display: `browser`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
 }
